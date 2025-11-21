5e9a7a9133c133f162b1684300d58658cbee0548
Rifq11 <rifqikamaluddin11@gmail.com>
2025-11-21 07:06:08 +0700

    change path

--- models/activity.go
+++ models/activity.go
@@ -1,6 +1,6 @@
 package models
 
-import "time"
+import "../Models/time"
 
 type ActivityLog struct {
 	ID            int       `gorm:"primaryKey;autoIncrement" json:"id"`
--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,6 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import "../Routes/github.com/gin-gonic/gin"
 
 func SetupRoutes(app *gin.Engine) {
 	api := app.Group("/api")
